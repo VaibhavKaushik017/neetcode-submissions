92c525b7493287e583054f47577ce2a33060d32d
Vaibhav Kaushik <128384880+VaibhavKaushik017@users.noreply.github.com>
2026-06-12 12:11:12 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        let map = new Map();
+
+        for (const char of s) {
+            if (map.has(char)) {
+                map.set(char, map.get(char) + 1);
+            } else {
+                map.set(char, 1);
+            }
+        }
+
+        for (const char of t) {
+            if (!map.has(char)) return false;
+
+            map.set(char, map.get(char) - 1);
+
+            if (map.get(char) === 0) {
+                map.delete(char);
+            }
+        }
+
+        return map.size === 0;
+    }
+}
